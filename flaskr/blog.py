a607c05f3018d3c6f20c564dc47697744fad723c
Adethya S Nair <adethyasanjay@gmail.com>
2022-12-27 19:01:07 +0530

    Create function written

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -7,3 +7,14 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, autho_id, username'
+        'FROM post p JOIN user u ON p.author_id = u.id'
+        'ORDER BY created DESC'
+    ).fetchall()
+
+    return render_template('blog/index.html', posts=posts)
