e2032f53b0e4ae864fcdf1297078b8e291fc3676
Adethya S Nair <adethyasanjay@gmail.com>
2022-12-27 18:38:52 +0530

    Blog blueprint created

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask 
 from . import db
 from . import auth
+from . import blog 
 
 
 def create_app(test_config = None):
@@ -33,5 +34,8 @@ def create_app(test_config = None):
 
     db.init_app(app)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     
     return app
