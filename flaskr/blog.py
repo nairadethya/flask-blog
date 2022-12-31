de8df82a225a4820702f21c0d081de8095413a0b
Adethya S Nair <adethyasanjay@gmail.com>
2022-12-31 13:12:59 +0530

    Update blueprint added

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -43,3 +43,33 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        'FROM post p JOIN user u ON p.author_id=u.id'
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} does not exist")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+@bp.route('/<int:id>/update', methods = ('GET','POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required. '
+
+        
